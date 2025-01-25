a19936dc6ed2deaaa80ef62069f735a6560d6a39
jeanport <pierreanatc522@gmail.com>
2025-01-25 14:10:11 -0300

    Created class UserReposity

--- src/main/java/com/jean/user_service/repository/UserRepository.java
+++ src/main/java/com/jean/user_service/repository/UserRepository.java
@@ -1,6 +1,42 @@
 package com.jean.user_service.repository;
 
+import com.jean.user_service.domain.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public class UserRepository {
 
-    
+    private final UserData data;
+
+    public UserRepository(UserData data) {
+        this.data = data;
+    }
+
+    public List<User> findAll() {
+        return data.getUsers();
+    }
+
+    public List<User> findByName(String name) {
+        return data.getUsers().stream().filter(user -> user.getFirstName().equalsIgnoreCase(name)).toList();
+    }
+
+    public Optional<User> findById(Long id) {
+        return data.getUsers().stream().filter(user -> user.getId().equals(id)).findFirst();
+    }
+
+    public User create(User user) {
+        data.getUsers().add(user);
+        return user;
+    }
+
+    public void remove(User user) {
+        data.getUsers().remove(user);
+    }
+    public void update(User user) {
+        remove(user);
+        data.getUsers().add(user);
+    }
 }
