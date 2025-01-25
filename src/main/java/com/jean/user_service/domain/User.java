767611556e27532d668257dfda5ae9d20de5e95b
jeanport <pierreanatc522@gmail.com>
2025-01-25 11:18:28 -0300

    Concluindo a classe de domain User

---
+++ src/main/java/com/jean/user_service/domain/User.java
@@ -0,0 +1,17 @@
+package com.jean.user_service.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+
+}
