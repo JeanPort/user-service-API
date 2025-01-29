16d4ad6cb85c73ab28630f2d1b19867d9f051cd6
jeanport <pierreanatc522@gmail.com>
2025-01-29 17:05:40 -0300

    Classes dto de Profile

--- src/main/java/com/jean/user_service/response/ProfileGetResponse.java
+++ src/main/java/com/jean/user_service/response/ProfileGetResponse.java
@@ -1,9 +1,18 @@
-package com.jean.user_service.request;
+package com.jean.user_service.response;
 
-public class ProfilePostRequest {
+public class ProfileGetResponse {
 
+    private Long id;
     private String name;
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
